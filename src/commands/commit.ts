05d9c707b12289f1f3e254b1533f0e6f40d3935b
olix3001 <olimioli9@gmail.com>
2022-04-19 20:53:02 +0200

    🔨 Fix indents

--- src/commands/commit.ts
+++ src/commands/commit.ts
@@ -7,90 +7,97 @@ import nameSuggestion from "../utils/nameSuggestion";
 import inquirer from "inquirer";
 
 export default (program: Command) => {
-	program
-		.command("commit")
-		.description("Commit changes in the index")
-		.option("-A, --add", "Add all files to the index (eg. 'git add .')")
-		.option("-S, --sign [key]", "Sign commit using SSH key")
-		.option(
-			"-H, --header <header>",
-			"Commit header for suggested name (works only without message option and commits uses suggestion automatically)"
-		)
-		.option("-m, --message <message>", "Commit message / description")
-		.action(async (options: any) => {
-			const git: SimpleGit = simpleGit(process.cwd());
+    program
+        .command("commit")
+        .description("Commit changes in the index")
+        .option("-A, --add", "Add all files to the index (eg. 'git add .')")
+        .option("-S, --sign [key]", "Sign commit using SSH key")
+        .option(
+            "-H, --header <header>",
+            "Commit header for suggested name (works only without message option and commits uses suggestion automatically)"
+        )
+        .option("-m, --message <message>", "Commit message / description")
+        .action(async (options: any) => {
+            const git: SimpleGit = simpleGit(process.cwd());
 
-			// check if repository exists
-			if (!(await git.checkIsRepo())) {
-				signale.error("There is no repository in current directory");
-				return;
-			}
+            // check if repository exists
+            if (!(await git.checkIsRepo())) {
+                signale.error("There is no repository in current directory");
+                return;
+            }
 
-			// add files to the index
-			if (options.add) {
-				if (!(await addAll())) {
-					signale.error("Could not add files to the index");
-					return;
-				}
-			}
+            // add files to the index
+            if (options.add) {
+                if (!(await addAll())) {
+                    signale.error("Could not add files to the index");
+                    return;
+                }
+            }
 
-			var message = options.message;
+            var message = options.message;
 
-			if (!message) {
-				var header = options.header;
-				if (!options.header) {
-					signale.info(
-						"You have not provided a message nor header for this commit, so here is a suggestion:"
-					);
-					console.log((await nameSuggestion()).join("\n"));
+            if (!message) {
+                var header = options.header;
+                if (!options.header) {
+                    signale.info(
+                        "You have not provided a message nor header for this commit, so here is a suggestion:"
+                    );
+                    console.log((await nameSuggestion()).join("\n"));
 
-					const r = await inquirer.prompt([
-						{
-							type: "input",
-							name: "header",
-							message:
-								"Header for your commit ('-' to provide custom commit message or empty for no custom header)",
-						},
-					]);
+                    const r = await inquirer.prompt([
+                        {
+                            type: "input",
+                            name: "header",
+                            message:
+                                "Header for your commit ('-' to provide custom commit message or empty for no custom header)",
+                        },
+                    ]);
 
-					header = r.header;
+                    header = r.header;
 
-					if (r.header === "-") {
-						message = (
-							await inquirer.prompt([
-								{
-									type: "editor",
-									name: "message",
-									message: "Message for your commit",
-								},
-							])
-						).message;
-						header = null;
-					}
-				}
+                    if (r.header === "-") {
+                        message = (
+                            await inquirer.prompt([
+                                {
+                                    type: "editor",
+                                    name: "message",
+                                    message: "Message for your commit",
+                                },
+                            ])
+                        ).message;
+                        header = null;
+                    }
+                }
 
-				if (header !== null) {
-					message = await nameSuggestion(header === "" ? undefined : header);
+                if (header !== null) {
+                    message = await nameSuggestion(
+                        header === "" ? undefined : header
+                    );
 
-					if (message.length > 1) {
-						message = [message[0], message.slice(1).join("\n")];
-					}
-				}
-			}
+                    if (message.length > 1) {
+                        message = [message[0], message.slice(1).join("\n")];
+                    }
+                }
+            }
 
-			// commit changes
-			git
-				.commit(message)
-				.then((result) => {
-					signale.success("Commit successful!\n");
+            // commit changes
+            git.commit(message)
+                .then((result) => {
+                    signale.success("Commit successful!\n");
 
-					signale.info(`Changes:    ${chalk.yellow(result.summary.changes)}`);
-					signale.info(`Insertions: ${chalk.green(result.summary.insertions)}`);
-					signale.info(`Deletions:  ${chalk.red(result.summary.deletions)}`);
-				})
-				.catch((e) => {
-					signale.error("Could not commit changes");
-					console.log(e);
-				});
-		});
+                    signale.info(
+                        `Changes:    ${chalk.yellow(result.summary.changes)}`
+                    );
+                    signale.info(
+                        `Insertions: ${chalk.green(result.summary.insertions)}`
+                    );
+                    signale.info(
+                        `Deletions:  ${chalk.red(result.summary.deletions)}`
+                    );
+                })
+                .catch((e) => {
+                    signale.error("Could not commit changes");
+                    console.log(e);
+                });
+        });
 };
--- src/utils/nameSuggestion.ts
+++ src/utils/nameSuggestion.ts
@@ -2,68 +2,68 @@ import signale from "signale";
 import simpleGit, { SimpleGit } from "simple-git";
 
 export default async function generate(header?: string) {
-	const git: SimpleGit = simpleGit(process.cwd());
+    const git: SimpleGit = simpleGit(process.cwd());
 
-	if (!(await git.checkIsRepo())) {
-		return [];
-	}
+    if (!(await git.checkIsRepo())) {
+        return [];
+    }
 
-	var message: string[] = [];
-	if (header) message.push(header);
+    var message: string[] = [];
+    if (header) message.push(header);
 
-	// initial commit
-	if (
-		await git
-			.log({ maxCount: 1 })
-			.then(() => false)
-			.catch(() => true)
-	) {
-		message.push("🎉 Initial commit");
-	}
+    // initial commit
+    if (
+        await git
+            .log({ maxCount: 1 })
+            .then(() => false)
+            .catch(() => true)
+    ) {
+        message.push("🎉 Initial commit");
+    }
 
-	// added files
-	const addedFiles = (await git.status()).created;
-	if (addedFiles.length <= 3) {
-		for (let file of addedFiles) {
-			message.push(`➕ Created ${file}`);
-		}
-	} else {
-		if (addedFiles.length != 0)
-			message.push(`➕ Created ${addedFiles.length} files`);
-	}
+    // added files
+    const addedFiles = (await git.status()).created;
+    if (addedFiles.length <= 3) {
+        for (let file of addedFiles) {
+            message.push(`➕ Created ${file}`);
+        }
+    } else {
+        if (addedFiles.length != 0)
+            message.push(`➕ Created ${addedFiles.length} files`);
+    }
 
-	// changed files
-	const changedFiles = (await git.status()).modified;
-	if (changedFiles.length <= 3) {
-		for (let file of changedFiles) {
-			message.push(`🔨 Changed ${file}`);
-		}
-	} else {
-		if (changedFiles.length != 0)
-			message.push(`🔨 Changed ${changedFiles.length} files`);
-	}
+    // changed files
+    const changedFiles = (await git.status()).modified;
+    if (changedFiles.length <= 3) {
+        for (let file of changedFiles) {
+            message.push(`🔨 Changed ${file}`);
+        }
+    } else {
+        if (changedFiles.length != 0)
+            message.push(`🔨 Changed ${changedFiles.length} files`);
+    }
 
-	// removed files
-	const removedFiles = (await git.status()).deleted;
-	if (removedFiles.length <= 3) {
-		for (let file of removedFiles) {
-			message.push(`➖ Removed ${file}`);
-		}
-	} else {
-		if (removedFiles.length != 0)
-			message.push(`➖ Removed ${removedFiles.length} files`);
-	}
+    // removed files
+    const removedFiles = (await git.status()).deleted;
+    if (removedFiles.length <= 3) {
+        for (let file of removedFiles) {
+            message.push(`➖ Removed ${file}`);
+        }
+    } else {
+        if (removedFiles.length != 0)
+            message.push(`➖ Removed ${removedFiles.length} files`);
+    }
 
-	// renamed files
-	const renamedFiles = (await git.status()).renamed;
-	if (renamedFiles.length <= 3) {
-		for (let file of renamedFiles) {
-			message.push(`🏷️ Renamed ${file.from} to ${file.to}`);
-		}
-	} else {
-		if (renamedFiles.length != 0)
-			message.push(`🏷️ Renamed ${renamedFiles.length} files`);
-	}
+    // renamed files
+    const renamedFiles = (await git.status()).renamed;
+    if (renamedFiles.length <= 3) {
+        for (let file of renamedFiles) {
+            message.push(`🏷️ Renamed ${file.from} to ${file.to}`);
+        }
+    } else {
+        if (renamedFiles.length != 0)
+            message.push(`🏷️ Renamed ${renamedFiles.length} files`);
+    }
 
-	return message;
+    return message;
 }
